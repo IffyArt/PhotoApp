a17ef56ba12eed8cd367018b457d5f4027519744
IffyArt <iffyart1234@gmail.com>
2017-12-25 15:48:14 +0800

    update Keep

---
+++ KeepthinkWeb/main.jsx
@@ -0,0 +1,27 @@
+/** @jsx React.DOM */
+
+var MyComponent = React.createClass({
+  handleClick: function() {
+    // Explicitly focus the text input using the raw DOM API.
+    this.refs.myTextInput.getDOMNode().focus();
+  },
+  render: function() {
+    // The ref attribute adds a reference to the component to
+    // this.refs when the component is mounted.
+    return (
+      <div>
+        <input type="text" ref="myTextInput" />
+        <input
+          type="button"
+          value="Focus the text input"
+          onClick={this.handleClick}
+        />
+      </div>
+    );
+  }
+});
+
+React.renderComponent(
+  <MyComponent />,
+  document.getElementById('example')
+);
